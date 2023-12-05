1f7303e77e902519874f527f6e2069eac1112554
Simon Ritz <simonritz9@gmail.com>
2023-12-05 15:24:09 +0100

    Zeile zu Array

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ document.getElementById("button").addEventListener("click", function () {
 
     let words = text.split("\n");
     console.log(words);
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i].split(" ");
-    }
-    console.log(words);
+
+
 });
