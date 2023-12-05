77a76936248bd92903bbe809e6c07e504a2aa7ae
Luke-2005 <luke.essing@web.de>
2023-12-05 14:15:11 +0100

    sentences splitt

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.getElementById("button").addEventListener("click", function () {
     let text = document.getElementById("text").value;
     console.log(text);
+
+    let words = text.split("\n");
+    console.log(words);
 });
