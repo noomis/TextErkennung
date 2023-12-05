25cb8e3b0547028aa788cfa5dcaa9809b699a3cd
Simon Ritz <simonritz9@gmail.com>
2023-12-05 16:54:39 +0100

    fix3

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ document.getElementById("button").addEventListener("click", function () {
     let text = document.getElementById("text").value;
     console.log(text);
 
-    words = text.split("\n");
+    let words = text.split("\n");
     console.log(words);
 
+    words.array.forEach(element => {
 
+        checkW3W(element);
+        checkURL(element);
+        checkMail(element);
+    
+    });
 });
 
-// words.array.forEach(element => {
 
-//     checkW3W(element);
-//     checkURL(element);
-//     checkMail(element);
-
-// });
 
 function checkW3W(inputLine) {
     
