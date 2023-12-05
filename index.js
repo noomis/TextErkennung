2b83e57b950d92f16f82c7bb9340ce7c11f56569
Simon Ritz <simonritz9@gmail.com>
2023-12-05 16:54:10 +0100

    fix2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.getElementById("button").addEventListener("click", function () {
     let text = document.getElementById("text").value;
     console.log(text);
 
-    let words = text.split("\n");
+    words = text.split("\n");
     console.log(words);
 
 
