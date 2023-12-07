b5f8ad86096473dd8892b11730437ba0669883e3
Simon Ritz <simonritz9@gmail.com>
2023-12-07 14:44:28 +0100

    Kommentare hinzugefügt

--- index.js
+++ index.js
@@ -191,17 +191,24 @@ function checkMail(inputLine) {
         const element = lineWords[index];
         let wordChars = element.split("");
 
-        knownTLD.forEach(tld => {
-            if (element.endsWith("." + tld)) {
+        knownTLD.forEach(tld => {           
+            if (element.endsWith("." + tld)) {  // Checkt ob TLD vorhanden ist
                 hasTLD = true;
             }
         });
 
-        if (element.startsWith('@')) {
+        if (element.startsWith('@')) { // Checkt String mit @ beginnt
             continue wordLoop;
           }
           else {
             wordProb += 5;
+        }
+
+        if (element.length < 6) {   // Checkt ob mindestens 6 Zeichen vorhanden sind
+            continue wordLoop;
+          }
+          else {
+            wordProb += 10;
         }
 
         charLoop: for (let i = 0; i < wordChars.length; i++) {
@@ -216,6 +223,7 @@ function checkMail(inputLine) {
                 dotHit.push(i);
             }
         }
+
         if (atHit.length !== 1) {   // checkt ob genau ein @ vorhanden ist.
             continue wordLoop;
         }
@@ -229,7 +237,7 @@ function checkMail(inputLine) {
         else {
             wordProb += 5;
         }
-        if (dotHit[dotHit.length] - atHit[0] <= 1) {    // checkt ob (x@y.de) y mindestens 1 character lang ist. 
+        if (dotHit[dotHit.length] - atHit[0] <= 1) {    // checkt ob y in (x@y.de) mindestens 1 character lang ist. 
             continue wordLoop;
         }
         else {
@@ -242,8 +250,8 @@ function checkMail(inputLine) {
             wordProb += 20;
         }
         if (index !== 0) {
-            let wordBefore = lineWords[index - 1].toLowerCase();
-            if (wordBefore.includes("mail")) {  // Checkt ob vor der Mail z.B. Mail: steht.
+            let wordBefore = lineWords[index - 1].toLowerCase(); // Checkt ob vor der Mail z.B. Mail: steht.
+            if (wordBefore.includes("mail")) {  
                 wordProb += 20;
             }
         }
