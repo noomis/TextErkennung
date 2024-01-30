9887da88c552ee81844dff3f30a7bc8ee7d83905
Luke-2005 <luke.essing@web.de>
2024-01-30 14:33:15 +0100

    s removed

--- Address.js
+++ Address.js
@@ -17,7 +17,7 @@ export class Address {
         return this.companyName;
     }
 
-    setCompanyNames(companyName) {
+    setCompanyName(companyName) {
         this.companyName = companyName;
     }
 
